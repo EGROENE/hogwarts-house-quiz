3ccc95046bef73b51f432d350ebd0ced0ba44cbd
Ethan Groene <ethangroene@gmail.com>
2023-10-30 22:29:53 -0600

    Give user ability to choose quiz length
    Also, add/edit/delete explanatory comments

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,21 @@
 import "./style.css";
 import { bgImages, housesInfo, allQuestions } from "./constants";
 
-// Functionality to change bg image
+// Functionality to change bg image:
 const setRandBG = (): void => {
   let randNum = Math.floor(Math.random() * bgImages.length);
   document.body.style.backgroundImage = "url(" + bgImages[randNum] + ")";
 };
+// Call as page initially loads:
 setRandBG();
+//////////////////////////////////////////////////////////////////
 
-let currentIndex: number = -1;
-
-// DOM Elements:
-const app = document.querySelector<HTMLElement>("#app");
-const greeting: HTMLElement = document.querySelector<HTMLElement>("#greeting")!;
-const startBtn: HTMLElement =
-  document.querySelector<HTMLElement>("#start-btn")!;
-
-const questionArea = document.querySelector<HTMLElement>("#question-area");
-
-const resultsArea = document.querySelector<HTMLElement>("#results-area");
-
-const shuffleArray = (array: [string, string][]) => {
+/* 
+  INFO ON shuffleArray():
+  @param: Any kind of array 
+  @returns: Shuffled version of passed-in array. Can be any type of array.
+*/
+const shuffleArray = (array: any[]): any[] => {
   for (let i = 1; i < array.length; i++) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -28,19 +23,47 @@ const shuffleArray = (array: [string, string][]) => {
   return array;
 };
 
-// Functionality to calculate points upon user answering question:
+// Define initial value of index:
+let currentIndex: number = -1;
+
+// DOM Elements:
+const app = document.querySelector<HTMLElement>("#app");
+
+const greeting: HTMLElement = document.querySelector<HTMLElement>("#greeting")!;
+
+const questionArea = document.querySelector<HTMLElement>("#question-area");
+
+const resultsArea = document.querySelector<HTMLElement>("#results-area");
+
+// FUNTIONALITY TO CALCULATE PONITS UPON USER ANSWERING QUESTION:
 const housesAndTheirTotals: { [key: string]: number } = {
   [`${housesInfo[0].houseName.toLowerCase()}`]: 0,
   [`${housesInfo[1].houseName.toLowerCase()}`]: 0,
   [`${housesInfo[2].houseName.toLowerCase()}`]: 0,
   [`${housesInfo[3].houseName.toLowerCase()}`]: 0,
 };
-
 const allocatePoints = (house: string, weight: number): void => {
   housesAndTheirTotals[`${house}`] += weight;
 };
+//////////////////////////////////////////////////////////////////
 
-// Call this on start button on init page & on next btn of each question
+// HANDLE USER'S SELECTION FOR LENGTH OF QUIZ:
+let quizLength: number = 4;
+const handleLengthSelection = (version: string): void => {
+  if (version === "short") {
+    quizLength = 2;
+  }
+};
+//////////////////////////////////////////////////////////////////
+
+/* 
+  INFO ON proceed():
+  @duty1: Increment currentIndex by 1
+  @duty2: Set a random background image.
+  @duty3: Handle proceeding to first & subsequent questions, and to results page.
+  @returns nothing
+*/
+// Call this onClick of "long" & "short" version buttons on init page & on "next" btn of each question.
 // Maybe divide this into toNextQuestion & toResults functions or something to make it more concise, like making separate func to populate innerHTML
 const proceed = (): void => {
   currentIndex++;
@@ -48,7 +71,7 @@ const proceed = (): void => {
   setRandBG();
 
   // If current question exists:
-  if (allQuestions[currentIndex]) {
+  if (questionsArray[currentIndex]) {
     if (currentIndex === 0) {
       // Upon moving to first question, remove greeting from DOM, display question area:
       app!.removeChild(greeting);
@@ -56,29 +79,30 @@ const proceed = (): void => {
     }
 
     // Create button for every answer option of current question:
-    // Must be added to innerHTML as a string
+    // These must be added to innerHTML as a string
     let answerOptions: string = "";
     const randomizedOptions = shuffleArray(
-      Object.entries(allQuestions[currentIndex].answers)
+      Object.entries(questionsArray[currentIndex].answers)
     );
     for (const option of randomizedOptions) {
       answerOptions += `<button class="option-btn" id="${option[0]}">${option[1]}</button>`;
     }
 
     // Clear info from previous question:
-    if (currentIndex > 0 && currentIndex !== allQuestions.length) {
+    if (currentIndex > 0 && currentIndex !== questionsArray.length) {
       questionArea!.innerHTML = "";
     }
 
     // Populate questionArea with info from current question:
     questionArea!.innerHTML += `
         <div class="question">
-        <header>${allQuestions.indexOf(allQuestions[currentIndex]) + 1} / ${
-      allQuestions.length
+        <header>${questionsArray.indexOf(questionsArray[currentIndex]) + 1} / ${
+      questionsArray.length
     }
         </header>
+        </header>${quizLength}</header>
         <header>
-        ${allQuestions[currentIndex].question}
+        ${questionsArray[currentIndex].question}
         </header>
         <div id="options-area">
         ${answerOptions}
@@ -90,14 +114,14 @@ const proceed = (): void => {
     const answerButtons = document.querySelectorAll(".option-btn");
     for (const button of answerButtons) {
       button.addEventListener("click", () => {
-        allocatePoints(button.id, allQuestions[currentIndex].weight);
+        allocatePoints(button.id, questionsArray[currentIndex].weight);
         proceed();
       });
     }
   }
 
   // Upon answering last question:
-  if (currentIndex === allQuestions.length) {
+  if (currentIndex === questionsArray.length) {
     const selectedHouse = (): {
       houseName: string;
       houseCrest: string;
@@ -132,4 +156,60 @@ const proceed = (): void => {
   }
 };
 
-startBtn.addEventListener("click", proceed);
+// FUNCTIONALITY TO GET ARRAY OF QUESTIONS USED IN GAME
+// Initiate empty questionsArray:
+let questionsArray: {
+  question: string;
+  answers: {
+    slytherin: string;
+    gryffindor: string;
+    ravenclaw: string;
+    hufflepuff: string;
+  };
+  weight: number;
+}[] = [];
+
+// Define question to go at end of quiz (what house the user prefers to be in):
+const questionOnHousePreference: {
+  question: string;
+  answers: {
+    slytherin: string;
+    gryffindor: string;
+    ravenclaw: string;
+    hufflepuff: string;
+  };
+  weight: number;
+} = allQuestions.filter(
+  (question) => question.question === "Which house do you hope to be in?"
+)[0];
+
+// Define array of questions not including the question on user's preference:
+let questionsNotOnHousePreference = shuffleArray(
+  allQuestions.filter(
+    (question) => question.question !== "Which house do you hope to be in?"
+  )
+);
+
+// Populate questionArray:
+const getQuestionArray = (): void => {
+  // Depending on length that the user selects, push questions that are not about user preference to questionsArray:
+  for (let i = 0; i < quizLength; i++) {
+    questionsArray.push(questionsNotOnHousePreference[i]);
+  }
+
+  // Append question on house preference to end of questionsArray:
+  questionsArray.push(questionOnHousePreference);
+};
+//////////////////////////////////////////////////////////////////
+
+// Add handleLengthSelection(), getQuestionArray(), & proceed() in EL onClick of length-selector btns:
+const lengthSelectorBtns = document.querySelectorAll<HTMLElement>(
+  ".length-selector-btn"
+);
+for (const btn of lengthSelectorBtns) {
+  btn.addEventListener("click", () => {
+    handleLengthSelection(btn.id);
+    getQuestionArray();
+    proceed();
+  });
+}
